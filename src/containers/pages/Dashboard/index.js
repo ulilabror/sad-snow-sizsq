dddb6dfe2b05c928a41782d29cf96248fbcd5c08
ulilabror <45628630+ulilabror@users.noreply.github.com>
2020-07-26 23:05:47 +0700

    sad-snow-sizsq

---
+++ src/containers/pages/Dashboard/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Register extends Component {
+  render() {
+    return (
+      <div>
+        <p> Register page </p>
+        <button>Go to Login page</button>
+        <button>Go to Dashboard Page</button>
+      </div>
+    );
+  }
+}
+
+export default Register;
