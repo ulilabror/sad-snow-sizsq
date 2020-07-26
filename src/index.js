50f49e5cbef89eae0d285f1db599e27e68c80be4
ulilabror <45628630+ulilabror@users.noreply.github.com>
2020-07-26 22:43:23 +0700

    sad-snow-sizsq

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import firebase from "./config/firebase";
-import App from "./App";
+//import App from "./containers/pages/App";
 console.log(firebase);
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement
-);
+ReactDOM.render(<React.StrictMode />, rootElement);
